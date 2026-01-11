90bd2f9b1e3f6dca6fcc18d4f549cec642c07816
TysonTheEmber <101071342+TysonTheEmber@users.noreply.github.com>
2026-01-11 10:38:09 -0700

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -27,7 +27,7 @@ const config: Config = {
   url: 'https://invxrtedstudios.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/invxrtedstudios.github.io/',
 
   trailingSlash: true,
 
